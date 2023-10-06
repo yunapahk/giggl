28621e8e7f99e7b6351b2b8a4eb016cf01137aaf
Yuna Pahk <yunapahk@gmail.com>
2023-10-06 16:55:28 -0700

    navbar updated - icons added - work on responsiveness later

--- giggl/src/App.vue
+++ giggl/src/App.vue
@@ -1,26 +1,35 @@
 <template>
   <v-app>  
     <header>
-      <v-app-bar :elevation="2">
-        <v-spacer></v-spacer> <!-- This pushes the content to the center -->
-
+      <v-app-bar :elevation="2" class="thin-navbar">
+        
+        <!-- Home button on the left -->
         <router-link :to="{ name: 'dashboard' }">
-          <v-btn text>Dashboard</v-btn>
+          <v-btn color="white">
+            <i class="fa-solid fa-house small-icon" style="color: #ffffff;"></i>
+          </v-btn>
         </router-link>
+
+        <!-- Spacer to push the rest to the center -->
+        <v-spacer></v-spacer>
+
+        <!-- Rest of the buttons -->
         <router-link :to="{ name: 'bits' }">
-          <v-btn text>Bits</v-btn>
+          <v-btn color="white" text>Bits</v-btn>
         </router-link>
         <router-link :to="{ name: 'comedians' }">
-          <v-btn text>Comedians</v-btn>
+          <v-btn color="white" text>Comedians</v-btn>
         </router-link>
         <router-link :to="{ name: 'podcasts' }">
-          <v-btn text>Podcasts</v-btn>
+          <v-btn color="white" text>Podcasts</v-btn>
         </router-link>
         <router-link :to="{ name: 'tourdates' }">
-          <v-btn text>Tour Dates</v-btn>
+          <v-btn color="white" text>Tour Dates</v-btn>
         </router-link>
 
-        <v-spacer></v-spacer> <!-- Keeps the buttons centered -->
+        <!-- Spacer to ensure the center alignment -->
+        <v-spacer></v-spacer>
+
       </v-app-bar>
     </header>
     <div class="content">
@@ -28,6 +37,7 @@
     </div>
   </v-app>
 </template>
+
 
 <script>
 export default {}
@@ -40,10 +50,15 @@ header {
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 
-nav {
+.v-app-bar.thin-navbar {
+  height: 55px;
   display: flex;
-  justify-content: center;
-  gap: 1rem;
+  align-items: center;
+  background-color: rgb(89, 89, 95);
+  margin-bottom: 20px;
+}
+.v-btn {
+  padding: 0px 50px 50px 60px;
 }
 
 router-link {
@@ -67,4 +82,10 @@ main {
   max-width: 1200px;
   margin: auto;
 }
+
+.small-icon {
+  font-size: 16px;
+  
+}
+
 </style>
